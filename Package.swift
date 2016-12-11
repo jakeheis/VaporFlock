369733cd05a1331ca7df612f456d2a299dfdb8de
Jake Heiser <jakeheiser1@gmail.com>
2016-12-11 02:09:03 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "VaporFlock",
     dependencies: [
-        .Package(url: "https://github.com/jakeheis/Flock", majorVersion: 0, minor: 0)
+        .Package(url: "https://github.com/jakeheis/Flock", majorVersion: 0, minor: 1)
     ]
 )
